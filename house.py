1bb28c6036adc15c2df048d0c72f71faa78ac47f
Nasif <90343949+nasifrkz@users.noreply.github.com>
2023-11-12 19:00:02 +0600

    Choosing the house in the wizarding world

---
+++ house.py
@@ -0,0 +1,16 @@
+name = input("What is your name?")
+
+match name: 
+    case "Hary":
+        print("Grif")
+    case _:
+        print("hehe")
+        
+
+if name == "Harry" or name== "nasif" or name== "anas":
+    print("Grifindor")
+elif name == "Ron":
+    print("Grif")
+else:
+    print("Not elligible")
+    
